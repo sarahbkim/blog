aba5ff8847f4f7a510447f44651c5d5fb7cdb97b
Sarah Kim <pnkdaegee@gmail.com>
2013-09-28 10:20:04 -0700

    fonts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Blog::Application.configure do
 
 
   #Add the fonts path
-  config.assets.paths << Rails.root.join('app', 'assets', 'fonts') 
+  config.assets.paths << Rails.root.join('app', 'public', 'fonts') 
 
   #Precompile additional assets 
   config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
