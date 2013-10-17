0e777eeea2d9eeeb94c973a0c5d36606dc6ac94c
Sarah Kim <pnkdaegee@gmail.com>
2013-10-16 22:24:21 -0700

    update action added

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -7,21 +7,30 @@ class Admin::PostsController < ApplicationController
 	end
 
 	def new
-		@post = Post.new 
+		@post = Post.new
+
 	end
 
 	def create
 		@post = Post.new(post_params)
 
 		@post.save
-		redirect_to admin_posts_path 
+		redirect_to admin_post_path(@post)
 	end
 
 	def edit
 		@post = Post.find(params[:id])
-
 	end
 	
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(post_params)
+			redirect_to admin_post_path(@post)
+		else
+			render 'edit' 
+		end
+	end
 
 	def show
 		@post = Post.find(params[:id])
