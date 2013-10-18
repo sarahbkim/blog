90a691cd970b19b8f9ccda0427fcd22d6f14cd5b
Sarah Kim <pnkdaegee@gmail.com>
2013-10-17 23:04:05 -0700

    clean files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,4 @@ Blog::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.assets.precompile += %w( ckeditor/* )
-
 end
