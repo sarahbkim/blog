106da269b7cacd8c1a073bdbcf0b090866489d36
sarahkim <pnkdaegee@gmail.com>
2018-04-24 12:51:39 -0700

    test

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,8 @@
+# more comments
 class Admin::CategoriesController < ApplicationController
 def create
 	@post = Post.find(params[:post_id])
- 	@categories = @post.categories.create(params[:category].permit(:name)) 
+ 	@categories = @post.categories.create(params[:category].permit(:name))
 	redirect_to post_path(@post)
 end
 end
